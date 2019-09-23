4dd968c619acd4ceb9a2c7712bbfa1a3c6442868
omowunmi.ekun@xputertechnologies.com <omowunmi.ekun@xputertechnologies.com>
2019-09-23 10:45:50 +0100

    reviews page

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Review } from './review.model';
+import { ReviewsService } from './reviews.service';
+import { StarRatingComponent } from 'ng-starrating';
 
 @Component({
   selector: 'app-reviews',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReviewsComponent implements OnInit {
   Review:string;
-  constructor() { }
+  reviews: Review[];
+  constructor(private reviewService:ReviewsService ) { }
 
   ngOnInit() {
-  }
 
+   this.reviews = this.reviewService.getReviews();
+  }
+  onRate($event:{oldValue:number, newValue:number, starRating:StarRatingComponent}) {
+    alert(`Old Value:${$event.oldValue},
+      New Value: ${$event.newValue},
+      Checked Color: ${$event.starRating.checkedcolor},
+      Unchecked Color: ${$event.starRating.uncheckedcolor}`);
+  }
 }
